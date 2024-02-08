ddf6a767307186815025ff3c2edbdfd388282aad
Jeremy Adams <jeremy@dagger.io>
2024-02-08 08:47:41 -0800

    test2
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>

--- main.go
+++ main.go
@@ -15,3 +15,15 @@ func (m *Both) Test(ctx context.Context) (string, error) {
 		//Stderr(ctx)
 }
 
+// Run with: dagger call test2
+func (m *Both) Test2(ctx context.Context) string {
+	foo := dag.Container().
+		From("alpine:latest").
+		WithExec([]string{"sh", "-c", `(echo "This is stdout" ; echo "This is stderr" 1>&2)`})
+
+	sout, _ := foo.Stdout(ctx)
+	serr, _ := foo.Stderr(ctx)
+
+	return sout + "\n" + serr
+}
+
